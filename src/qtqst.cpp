631d8dec17c4e0d2dfebb7e4f229dd8174c89d45
Marisa Kirisame <soymilk@airmail.cc>
2018-03-30 19:24:28 -0400

    Instantiated timer clock in prep for gamelogic

--- src/qtqst.cpp
+++ src/qtqst.cpp
@@ -4,12 +4,16 @@
 
 #include "inputhandler.hpp"
 
+double secondsPerTick = 0.5;
+
 int main() {
   
   sf::RenderWindow window(sf::VideoMode(640, 480), "qtqst under construction");
   
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
+
+  sd::Clock clock;
 
   InputHandler handler(window, shape);
 
@@ -22,6 +26,12 @@ int main() {
       handler.handleInput(event);
     }
 
+    sf::Time dt = clock.restart();
+
+    //currentLevel.updateGameLogic(dt * secondsPerTick);
+
+    //currentLEvel.updateRendition(dt);
+    
     window.clear();
     window.draw(shape);
     window.display();
