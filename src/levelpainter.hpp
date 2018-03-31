fc905b48bc93ff1d43c57abe02609cadf12d3c9b
Marisa Kirisame <soymilk@airmail.cc>
2018-03-30 22:06:13 -0400

    Started the static levelpainter

---
+++ src/levelpainter.hpp
@@ -0,0 +1,16 @@
+#ifndef LEVEL_PAINTER_INCLUDE
+#define LEVEL_PAINTER_INCLUDE
+
+#include "level.hpp"
+
+class LevelPainter {
+
+public:
+
+  static Level* paintEmptyLevel(int width, int height);
+
+private:
+
+};
+
+#endif // LEVEL_PAINTER_INCLUDE
